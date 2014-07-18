819543d4e130a2119e4cebf1d47c2def56c309a1
Chris Stephens <cstephe@gmail.com>
2014-07-17 21:59:09 -0400

    separated timer into service so it can run in background

--- app/scripts/controllers/timer.js
+++ app/scripts/controllers/timer.js
@@ -8,50 +8,84 @@
  * Controller of the pomodairyApp
  */
 angular.module('pomodairyApp')
-    .service('timerService', function () {
-
-    })
-    .controller('TimerCtrl', function ($scope, $interval, $stateParams, moment, timerService) {
+    .service('timerService', function (moment, $interval) {
         var timer;
         var breakTime = 0.5;
         var taskTime = 1;
-        console.log($stateParams)
-        $scope.model = {
-            current: moment.duration(taskTime, 'm'),
-            break: moment.duration(breakTime, 'm'),
-            task: [],
-            completed: 0
-        };
-
         var getTimerDisplayData = function () {
             return [
                 {
                     key: "Break",
-                    y: $scope.model.break.asMilliseconds() || 0
+                    y: toReturn.model.break.asMilliseconds() || 0
                 },
                 {
                     key: "BreakPassed",
-                    y: moment.duration(breakTime, 'm').subtract($scope.model.break).asMilliseconds() || 0
+                    y: moment.duration(breakTime, 'm').subtract(toReturn.model.break).asMilliseconds() || 0
                 },
                 {
                     key: "Remaining",
-                    y: $scope.model.current.asMilliseconds() || 0
+                    y: toReturn.model.current.asMilliseconds() || 0
                 },
                 {
                     key: "Passed",
-                    y: moment.duration(taskTime, 'm').subtract($scope.model.current).asMilliseconds() || 0
+                    y: moment.duration(taskTime, 'm').subtract(toReturn.model.current).asMilliseconds() || 0
                 }
             ]
         };
-
+        var toggle = function () {
+            if (!timer) {
+                toReturn.model.timerState = "STARTED"
+                timer = $interval(function () {
+                    if (toReturn.model.current != 0) {
+                        toReturn.model.current.subtract(1, 's');
+                        toReturn.model.timerDisplay = getTimerDisplayData();
+                    } else if (toReturn.model.break != 0) {
+                        toReturn.model.break.subtract(1, 's');
+                        toReturn.model.timerDisplay = getTimerDisplayData();
+                    } else {
+                        alert('done mother fucker');
+                        toReturn.model.timerState = null;
+                        abandonTimer();
+                        toReturn.model.completed = toReturn.model.completed + 1;
+                    }
+                }, 1000);
+            } else {
+                toReturn.model.timerState = "PAUSED";
+                $interval.cancel(timer);
+                timer = null;
+            }
+        };
+        var abandonTimer = function () {
+            $interval.cancel(timer);
+            timer = null;
+            toReturn.model.current = moment.duration(taskTime, 'm');
+            toReturn.break = moment.duration(breakTime, 'm');
+            toReturn.model.timerDisplay = getTimerDisplayData();
+        };
+        var toReturn = {
+            model: {
+                timerState : null,
+                current: moment.duration(taskTime, 'm'),
+                break: moment.duration(breakTime, 'm'),
+                task: [],
+                completed: 0,
+                timerDisplay : {}
+            },
+            toggle : toggle,
+            abandon : abandonTimer
+        };
+        toReturn.model.timerDisplay = getTimerDisplayData();
+        return toReturn;
+    })
+    .controller('TimerCtrl', function ($scope, $stateParams, timerService) {
+        console.log($stateParams)
+        $scope.model = timerService.model;
         var colorArray = ['#ffb600', '#000', '#428bca', '#000'];
         $scope.colorFunction = function () {
             return function (d, i) {
                 return colorArray[i];
             };
         }
-        $scope.timerDisplay = getTimerDisplayData();
-
         $scope.xFunction = function () {
             return function (d) {
                 return d.key;
@@ -62,38 +96,6 @@ angular.module('pomodairyApp')
                 return d.y;
             };
         };
-        $scope.toggleTimer = function () {
-            if (!timer) {
-                $scope.timerState = "STARTED"
-                timer = $interval(function () {
-                    if ($scope.model.current != 0) {
-                        $scope.model.current.subtract(1, 's');
-                        $scope.timerDisplay = getTimerDisplayData();
-                    } else if ($scope.model.break != 0) {
-                        $scope.model.break.subtract(1, 's');
-                        $scope.timerDisplay = getTimerDisplayData();
-                    } else {
-                        alert('done mother fucker');
-                        $scope.timerState = null;
-                        $scope.abandonTimer();
-                        $scope.model.completed = $scope.model.completed + 1;
-                    }
-                }, 1000);
-            } else {
-                $scope.timerState = "PAUSED";
-                $interval.cancel(timer);
-                timer = null;
-            }
-        };
-        $scope.abandonTimer = function () {
-            $interval.cancel(timer);
-            timer = null;
-            $scope.model.current = moment.duration(taskTime, 'm');
-            $scope.model.break = moment.duration(breakTime, 'm');
-            $scope.timerDisplay = getTimerDisplayData();
-        };
-        $scope.$on('$destroy', function () {
-            $interval.cancel(timer);
-            timer = null;
-        });
+        $scope.toggleTimer = timerService.toggle;
+        $scope.abandonTimer =  timerService.abandon;
     });
